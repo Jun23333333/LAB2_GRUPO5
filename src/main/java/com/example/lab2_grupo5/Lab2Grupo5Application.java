96348204bab5f6b74cf0eb6aa031c847b072367c
MarceloJon <mrojasl@pucp.edu.pe>
2022-09-13 20:34:44 -0500

    base

---
+++ src/main/java/com/example/lab2_grupo5/Lab2Grupo5Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Grupo5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2Grupo5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab2_grupo5/Lab2Grupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2Grupo5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
